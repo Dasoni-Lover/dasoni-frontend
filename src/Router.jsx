7fb3de444c3aa5c665b9faf46674d62b761dc0ed
choidaye8170 <gracechoidaye@gmail.com>
2025-10-31 15:42:26 +0900

    feat: router 설정

---
+++ src/Router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, Router } from "react-router-dom";
+import App from "./App";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<App/>,
+        children:[
+            {index:true,element:<HomePage/>}
+        ]
+    }
+])
+
+export default router;
