3fbc582483b74a2a7d7374f340af431b7afd7e76
sunwoo-h <flexible8011@gmail.com>
2025-11-04 15:43:03 +0900

    feat: 게시글 상세조회 모달 퍼블리싱

--- src/components/photobox/MediumPhotoBox.jsx
+++ src/components/photobox/MediumPhotoBox.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-export const MediumPhotoBox = ({ src }) => {
-  return <Img src={src} alt="사진" />;
+export const MediumPhotoBox = ({ src, ...rest }) => {
+  return <Img src={src} alt="사진" {...rest} />;
 };
 
 const Img = styled.img`
   width: 22.25rem;
   height: 22.25rem;
   border-radius: 5px;
-  border: 2px solid #E9E9E9;
+  border: 2px solid #e9e9e9;
   box-sizing: border-box;
+  cursor: pointer;
 `;
