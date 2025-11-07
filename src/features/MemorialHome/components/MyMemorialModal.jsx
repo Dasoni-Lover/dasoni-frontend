a2d2aa725c0f335bb31e5ca8ce8690dccc92ea80
choidaye8170 <gracechoidaye@gmail.com>
2025-11-07 22:39:24 +0900

    feat : 나의 추모관 개설 설정 완

--- src/features/MemorialHome/components/MyMemorialModal.jsx
+++ src/features/MemorialHome/components/MyMemorialModal.jsx
@@ -2,24 +2,21 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import { typo } from "../../../styles/tokens";
 import Button from "../../../components/Button";
-import { SidebarContext } from "../../../styles/GlobalStyle"; // ✅ Context import
+import { SidebarContext } from "../../../styles/GlobalStyle";
 
 export default function MyMemorialModal({ isOpen, onCreateClick }) {
-  const { isOpen: sidebarOpen } = useContext(SidebarContext); // ✅ 사이드바 상태 사용
+  const { isOpen: sidebarOpen } = useContext(SidebarContext);
 
   if (!isOpen) return null;
 
   return (
     <Overlay $sidebarOpen={sidebarOpen}>
       <ModalContainer>
-        <Title>
-          훗날, 남겨질 나의 소중한 사람들에게 꼭 전하고 싶은 진심이 있나요?
-        </Title>
+        <Title>훗날, 남겨질 나의 소중한 사람들에게 꼭 전하고 싶은 진심이 있나요?</Title>
         <SubText>
           나의 추모관을 개설하면 사랑하는 이들을 위해 안부를 남기고, <br />
           기억하고 싶은 내 삶의 순간을 기록할 수 있어요
         </SubText>
-
         <ButtonContainer>
           <Button onClick={onCreateClick} text="나의 추모관 개설하기" />
         </ButtonContainer>
@@ -34,17 +31,15 @@ const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.45);
   display: flex;
   align-items: center;
-  justify-content: center; /* ✅ 사이드바 열림 시 왼쪽으로 이동 */
+  justify-content: center;
   z-index: 3000;
   width: ${({ $sidebarOpen }) =>
     $sidebarOpen ? "calc(100% - 18.75rem)" : "100%"};
   height: calc(100% - 6.25rem);
   margin-top: 6.25rem;
-  margin-left:  ${({ $sidebarOpen }) =>
-    $sidebarOpen ? "18.75rem" : "0"};
+  margin-left:  ${({ $sidebarOpen }) => ($sidebarOpen ? "18.75rem" : "0")};
   transition: all 0.3s ease;
   pointer-events: all;
-  z-index: 5;
 `;
 
 const ModalContainer = styled.div`
@@ -52,7 +47,6 @@ const ModalContainer = styled.div`
   width: 52.4375rem;
   flex-direction: column;
   align-items: center;
-  margin-left: ${({ $sidebarOpen }) => ($sidebarOpen ? "12rem" : "0")};
   transition: all 0.3s ease;
 `;
 
