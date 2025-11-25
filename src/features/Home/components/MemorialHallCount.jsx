2f01084ecf10dd5541bcf251ee2a37a9fc50aa89
choidaye8170 <gracechoidaye@gmail.com>
2025-11-26 03:14:59 +0900

    feat : 홈 추모관 검색 인풋 생성

--- src/features/Home/components/MemorialHallCount.jsx
+++ src/features/Home/components/MemorialHallCount.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { color, typo } from "../../../styles/tokens";
 import { useNavigate } from "react-router-dom";
 import Button from "../../../components/Button";
+import Icon from "../assets/icon-search2.svg";
 
 export const MemorialHallCount = ({ count = 0, tab = 0 }) => {
   const nav = useNavigate();
@@ -22,6 +23,13 @@ export const MemorialHallCount = ({ count = 0, tab = 0 }) => {
   return (
     <Wrapper>
       <Text>{text}</Text>
+      <Container>
+        <InputWrapper>
+        <StyledInput placeholder="이름을 입력하세요" />
+        <SearchButton type="button">
+          <img src={Icon} alt="search" />
+        </SearchButton>
+      </InputWrapper>
 
       <Button
         text={buttonText}
@@ -31,6 +39,7 @@ export const MemorialHallCount = ({ count = 0, tab = 0 }) => {
         color="white"
         icon="add"
       />
+      </Container>
     </Wrapper>
   );
 };
@@ -42,9 +51,58 @@ const Wrapper = styled.div`
   align-items: center;
   align-self: stretch;
   justify-content: space-between;
+  gap: 1rem;
 `;
+const Container=styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 1.25rem;
+`
 
 const Text = styled.div`
   ${typo("h4")};
   color: ${color("black.30")};
+`;
+
+
+const InputWrapper = styled.div`
+display: flex;
+width: 17.3125rem;
+height: 3.25rem;
+padding: 0.5rem 1rem;
+align-items: center;
+border-radius: 0.375rem;
+border: 2px solid var(--5, #E9E9E9);
+background: var(--0, #FFF);
+  box-sizing: border-box;
+gap: 0.625rem;
+`;
+
+const StyledInput = styled.input`
+  width: 12.4375rem;
+  height: 100%;
+  border: none;
+  background: transparent;
+  ${typo("h4")};
+  outline: none;
+
+  &::placeholder {
+    color: ${color("black.10")};
+  }
+`;
+
+const SearchButton = styled.button`
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  background-color: transparent;
+
+  width: 1.5rem;
+  height: 1.5rem;
+
+
 `;
