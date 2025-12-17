dffac3bf9a4d3cba229c3d695f12329c20339263
sunwoo-h <flexible8011@gmail.com>
2025-12-17 19:07:21 +0900

    design: BluePart 수정

--- src/features/Landing/components/BluePart.jsx
+++ src/features/Landing/components/BluePart.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import FeatureInfo from "./FeatureInfo";
 import styled from "styled-components";
-import ImgMyHall from "../assets/img-myhall-example.svg";
+import ImgMyHall1 from "../assets/img-myhall-example-1.png";
+import ImgMyHall2 from "../assets/img-myhall-example-2.svg";
 
 export default function BluePart() {
   return (
@@ -12,13 +13,16 @@ export default function BluePart() {
           main="훗날, 남겨질 소중한 사람들에게 꼭 전하고 싶은 진심이 있나요?"
           sub={`나의 추모관을 개설해 보세요\n기억하고 싶은 내 삶의 순간들을 기록하고\n사랑하는 사람들에게 하고 싶었던 말을 편지로 남길 수 있어요`}
         />
-        <MyhallImg src={ImgMyHall} />
+
+        <MyhallImg1 src={ImgMyHall1} />
+        <MyhallImg2 src={ImgMyHall2} />
       </MyHallSection>
     </>
   );
 }
 
 const MyHallSection = styled.div`
+  position: relative;
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -26,7 +30,20 @@ const MyHallSection = styled.div`
   margin-bottom: 8rem;
 `;
 
-const MyhallImg = styled.img`
+const MyhallImg1 = styled.img`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-85%);
+  bottom: 0px;
+  width: 48rem;
+  z-index: 1;
+`;
+
+const MyhallImg2 = styled.img`
+  position: absolute;
+  left: 50%;
+  transform: translateX(0%);
   margin-top: 8rem;
-  width: 91.1875rem;
+  width: 56rem;
+  z-index: 2;
 `;
